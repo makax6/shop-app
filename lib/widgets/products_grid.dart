02fc77377b000823ce49651b372864e558d5b637
hachem makaveli <hachem500@hotmail.com>
2020-03-23 20:24:05 +0100

    shop-state-management

---
+++ lib/widgets/products_grid.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+import '../providers/products.dart';
+import './product_item.dart';
+import 'package:provider/provider.dart';
+
+class ProductsGrid extends StatelessWidget {
+  final bool showFavs;
+  ProductsGrid(this.showFavs);
+  @override
+  Widget build(BuildContext context) {
+    final productsData = Provider.of<Products>(context);
+    final products = showFavs ? productsData.favoriteItems : productsData.items;
+    return GridView.builder(
+      padding: const EdgeInsets.all(10.0),
+      itemCount: products.length,
+      itemBuilder: (ctx, i) => ChangeNotifierProvider.value(
+        // create: (c) => products[i],
+        value: products[i],
+        child: ProductItem(
+            // products[i].id,
+            // products[i].title,
+            // products[i].imageUrl,
+            ),
+      ),
+      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+        crossAxisCount: 2, //number of column
+        childAspectRatio: 3 / 2, //child aspect ratio
+        crossAxisSpacing: 10, //spacing between colums
+        mainAxisSpacing: 10, // spacing between rows
+      ),
+    );
+  }
+}
